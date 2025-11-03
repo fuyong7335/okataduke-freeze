4994b781906e3f00cdb2e0eea92a94fbfb6648d7
fuyong7335 <yukie353yukie@gmail.com>
2025-11-03 22:18:58 +0900

    Add files via upload

---
+++ okataduke-freezemain_freeze.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("🧠 思考フリーズタイプのあなたへ")
+st.write("💡 動けない原因は情報過多かも。まずは1ヶ所に集中してみましょう！")
+st.markdown("▼アドバイスの続きは、無料メール講座でお届けしています👇")
+st.markdown("[📩 メールで受け取る（無料）](https://www.reservestock.jp/subscribe/221907)")
